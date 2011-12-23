9caa61d4a2b82ed37224ab135a1b683808626caa
Brad Griffin <bgriffin20@gmail.com>
2011-12-22 20:42:30 -0600

    Finished first cut of the User model

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem 'jquery-rails'
 
 group :development do
   gem 'rspec-rails', '2.6.1'
+  gem 'annotate', '2.4.0'
 end
 
 group :test do
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
-  
+  resources :users
+
+  match '/signup', :to => 'users#new'  
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
   match '/help',    :to => 'pages#help'
@@ -9,7 +11,6 @@ SampleApp::Application.routes.draw do
   get "pages/contact"
   get "pages/about"
   get "pages/help"
-  get "users/new"
   
   root :to => 'pages#home'
   
