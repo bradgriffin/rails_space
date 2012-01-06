c4c07113cc4ed8b0acfe8ca713a5369343971830
Brad Griffin <bgriffin20@gmail.com>
2012-01-05 22:52:15 -0600

    Chapter 7 Excercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,44 @@ class User < ActiveRecord::Base
     return user if user.has_password?(submitted_password)
   end
 
+  def User.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+    return nil
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    if user.nil?
+      nil
+    elsif user.has_password?(submitted_password)
+      user
+    else
+      nil
+    end
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    if user.nil?
+      nil
+    elsif user.has_password?(submitted_password)
+      user
+    end
+  end
+
+  def self.authenticate(email, submitted_password)
+    user = find_by_email(email)
+    user && user.has_password?(submitted_password) ? user : nil
+  end
+
   private
 
     def encrypt_password
